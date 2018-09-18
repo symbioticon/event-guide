6bec2e757e6d302cbe7cdf2aa20eaad451b995af
Swen Jung <sju@starfinanz.de>
2018-09-18 11:47:32 +0200

    added files

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,24 @@
-module.exports = function(app){
+module.exports = function (app) {
 
-  app.get('/voting/new', function(req, res){
+  app.get('/voting/new', function (req, res) {
     // Render the standings template and pass the photos
     res.render('voting_new', {});
   });
 
-  app.post('/api/v1/voting', function(req, res){
+  app.post('/api/v1/voting', function (req, res) {
     var uuid = '12314124515125125125125';
-    res.redirect('/voting/'+uuid);
+    res.redirect('/voting/' + uuid);
   });
 
-  app.get('/voting/:uuid/vote', function(req, res){
+  app.get('/voting/:uuid/vote', function (req, res) {
     res.render('voting_vote', {});
   });
 
-  app.get('/voting/:uuid/view', function(req, res){
+  app.get('/voting/:uuid/view', function (req, res) {
     res.render('voting_view', {});
   });
 
-  app.get('/voting/:uuid', function(req, res){
+  app.get('/voting/:uuid', function (req, res) {
     res.render('voting_view', {});
   });
 };
