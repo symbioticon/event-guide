794f88685b8c0c6d67cb7d6480b0f243bcacb7d9
Jan-Marc <jan-marc@web.de>
2018-09-18 11:43:29 +0200

    updated express-handlebars version

--- src/config.js
+++ src/config.js
@@ -4,31 +4,32 @@
 
 // Include the handlebars templating library
 var handlebars = require('express-handlebars'),
-  express = require('express');
+    express = require('express'),
+    bodyParser = require('body-parser');
 
 // Require()-ing this module will return a function
 // that the index.js file will use to configure the
 // express application
 
-module.exports = function(app){
+module.exports = function (app) {
 
-  // Register and configure the handlebars templating engine
-  app.engine('html', handlebars({
-    defaultLayout: 'main',
-    extname: ".html",
-    layoutsDir: __dirname + '/views/layouts'
-  }));
+    // Register and configure the handlebars templating engine
+    app.engine('html', handlebars({
+        defaultLayout: 'main',
+        extname: ".html",
+        layoutsDir: __dirname + '/views/layouts'
+    }));
 
-  // Set .html as the default template extension
-  app.set('view engine', 'html');
+    // Set .html as the default template extension
+    app.set('view engine', 'html');
 
-  // Tell express where it can find the templates
-  app.set('views', __dirname + '/views');
+    // Tell express where it can find the templates
+    app.set('views', __dirname + '/views');
 
-  // Make the files in the public folder available to the world
-  app.use(express.static(__dirname + '/../public'));
+    // Make the files in the public folder available to the world
+    app.use(express.static(__dirname + '/../public'));
 
-  // Parse POST request data. It will be available in the req.body object
-  app.use(express.urlencoded());
+    // Parse POST request data. It will be available in the req.body object
+    app.use(bodyParser.urlencoded({extended: true}));
 
 };
